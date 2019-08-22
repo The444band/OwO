f61e2768dcc708b8d182ad9c6eab4cb69e2db6d0
The444band <54388925+The444band@users.noreply.github.com>
2019-08-21 22:13:09 -0700

    Add files via upload

---
+++ z-loadscreen-master/config.js
@@ -0,0 +1,17 @@
+/* Need Help? Join my discord @ discord.gg/9MdAw98 */
+var config = {
+    /* In milliseconds */
+    "transitionInterval": 1250,
+    "imgInterval": 8000,
+    "music": true,
+    /* Insert the Youtube Video ID below */
+    "videoID": "F_1uW4qxYrI",
+    /* Music Volume (0%-100%) | Lower is Quieter, Higher is Louder */
+    "musicVolume": 40,
+    "text": {
+        "title": "TCOS RP",
+        "link": "discord.gg/9MdAw98",
+    },
+    /* You dont have to include the '/imgs/' dir AND ALSO REMEMBER TO ADD THESE IMGS IN YOUR __resource.lua*/ 
+    "images": ['bg0.png', 'bg1.png', 'bg2.png', 'bg3.png', 'bg4.png', 'bg5.png', 'bg6.png', 'bg7.png']
+}
